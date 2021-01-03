cc3e2154454df5857b0a19058fa08b0ad2d81fc6
Veggie321 <74619174+Veggie321@users.noreply.github.com>
2021-01-03 03:41:15 -0500

    Update Game.js

--- Snake/Game.js
+++ Snake/Game.js
@@ -9,7 +9,7 @@ let lastRenderTime= 0
 function start(currentTime){ 
 if(gameOver){
     if(confirm("You lose! Don't lose... duh. Press OK to try again")){
-        window.location ='https://www.veggie321.codes/Snake/Game_Display.html'
+        window.location.href ="https://www.veggie321.codes/Snake/Game_Display.html"
     }
     return
 }
